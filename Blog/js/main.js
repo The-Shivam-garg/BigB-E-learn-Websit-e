55e9f32bd19f10ae494079590845563ce0fb66dc
rohit-raj25 <rohitraj250310@gmail.com>
2022-02-09 23:52:51 +0530

    Add sidebar in blogs page

--- Blog/js/main.js
+++ Blog/js/main.js
@@ -1,48 +1,59 @@
 const responsive = {
-    0: {
-        items: 1
-    },
-    320: {
-        items: 1
-    },
-    560: {
-        items: 2
-    },
-    960: {
-        items: 3
-    }
-}
+  0: {
+    items: 1,
+  },
+  320: {
+    items: 1,
+  },
+  560: {
+    items: 2,
+  },
+  960: {
+    items: 3,
+  },
+};
 
 $(document).ready(function () {
+  $nav = $(".nav");
+  $toggleCollapse = $(".toggle-collapse");
 
-    $nav = $('.nav');
-    $toggleCollapse = $('.toggle-collapse');
+  /** click event on toggle menu */
+  $toggleCollapse.click(function () {
+    $nav.toggleClass("collapse");
+  });
 
-    /** click event on toggle menu */
-    $toggleCollapse.click(function () {
-        $nav.toggleClass('collapse');
-    })
+  $(".navbar-show-btn").click(() => {
+    $(".navbar-collapse").addClass("showNavbar");
+  });
 
-    // owl-crousel for blog
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        autoplay: false,
-        autoplayTimeout: 3000,
-        dots: false,
-        nav: true,
-        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
-        responsive: responsive
-    });
+  $(".navbar-hide-btn").click(() => {
+    $(".navbar-collapse").removeClass("showNavbar");
+  });
 
+  // owl-crousel for blog
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 3000,
+    dots: false,
+    nav: true,
+    navText: [
+      $(".owl-navigation .owl-nav-prev"),
+      $(".owl-navigation .owl-nav-next"),
+    ],
+    responsive: responsive,
+  });
 
-    // click to scroll top
-    $('.move-up span').click(function () {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 1000);
-    })
+  // click to scroll top
+  $(".move-up span").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      1000
+    );
+  });
 
-    // AOS Instance
-    AOS.init();
-
+  // AOS Instance
+  AOS.init();
 });
