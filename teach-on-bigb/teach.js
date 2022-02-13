0e61bf646e9f4d45755090b10d952f3bf5c07cd8
soujanya hazra <soujanya0105@gmail.com>
2022-02-13 23:06:13 +0530

    required functions added

---
+++ teach-on-bigb/teach.js
@@ -0,0 +1,52 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+// ********************************************************
+
+let nums = document.querySelectorAll(".num-counter");
+let numSection = document.querySelector(".why-section");
+let started = false;
+
+window.onscroll = function () {
+    if (window.scrollY > 700) {
+        if (!started) {
+            nums.forEach((num) => startCount(num));
+        }
+        started = true;
+    }
+};
+
+function startCount(elem) {
+    let target = elem.dataset.target;
+    let count = setInterval(() => {
+        elem.textContent++;
+        if (elem.textContent == target) {
+            clearInterval(count);
+        }
+    }, 2500 / target);
+}
